bae806189ebdeead41cb1293701aa2dcd47697bb
renatodepsf <renatodepsf@gmail.com>
2023-02-24 14:18:32 -0300

    metodos atualizados

--- src/main/java/com/cadastro/db/controller/ClienteControlador.java
+++ src/main/java/com/cadastro/db/controller/ClienteControlador.java
@@ -4,6 +4,7 @@ import com.cadastro.db.exception.ResourceNotFoundException;
 import com.cadastro.db.model.Cliente;
 import com.cadastro.db.repository.ClienteRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,47 +12,40 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping
 public class ClienteControlador {
 
     @Autowired
     ClienteRepositorio repositorio;
 
-    @GetMapping("") //buscar todos
+    @GetMapping("/clientes") //buscar todos
     public List<Cliente> listaClientes() {
         return repositorio.findAll();
     }
 
-    @GetMapping("/{id}") //buscar por id
+    @GetMapping("clientes/{id}") //buscar por id
     public ResponseEntity<Cliente> buscarClientePorId(@PathVariable("id") Long id) {
         Cliente cliente = repositorio.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado para o id: " + id));
         return ResponseEntity.ok(cliente);
     }
 
-    @GetMapping("/{nome}")
-    public List<Cliente> buscarClientePorNome(@RequestParam(value="nome")String nome) {
-        return repositorio.findByNome(nome);
+    @GetMapping("clientes/nome")
+    public ResponseEntity<List<Cliente>> buscarClientePorNome(@RequestParam String nome) {
+        return new ResponseEntity<List<Cliente>>(repositorio.findByNome(nome), HttpStatus.MULTI_STATUS);
     }
-
-    @PostMapping //salvar clientes
+    @GetMapping("clientes/cpfcnpj")
+    public ResponseEntity<List<Cliente>> buscarClientePorCpfCnpj(@RequestParam String cpfcnpj) {
+        return new ResponseEntity<List<Cliente>>(repositorio.findByCpfcnpj(cpfcnpj), HttpStatus.MULTI_STATUS);
+    }
+    @PostMapping
     public Cliente criarCliente(@RequestBody Cliente cliente) {
         cliente.setDataInsercao(new Date());
         return repositorio.save(cliente);
     }
-
-    @DeleteMapping("{id}")
+    @DeleteMapping("/clientes/{id}")
     public void deletarClientePorId(@PathVariable("id") Long id) {
         repositorio.deleteById(id);
         Cliente cliente = new Cliente();
         cliente.setDataExclusao(new Date());
     }
-
-    /*
-    @GetMapping("clientes/IdNomeCpf")
-    public ResponseEntity<Cliente> buscarClientePorIdNomeCpf(Cliente cliente) throws Throwable {
-        if (cliente.getId() != null) {
-            cliente = (Cliente) repositorio.findAll().stream().filter(p -> p.getId() != null);
-        }
-        return ResponseEntity.ok(cliente);
-    }*/
 }
