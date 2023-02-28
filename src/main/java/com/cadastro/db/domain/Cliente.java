fb781d37a8a888539b2317fb8d68bd3a02ff1140
Renato <renatodepsf@gmail.com>
2023-02-28 10:13:23 -0300

    adicionado lomboK

--- src/main/java/com/cadastro/db/domain/Cliente.java
+++ src/main/java/com/cadastro/db/domain/Cliente.java
@@ -1,7 +1,12 @@
 package com.cadastro.db.domain;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "dbCadastro")
 public class Cliente {
@@ -12,71 +17,6 @@ public class Cliente {
     private String cpfcnpj;
     private String dtNascimento;
     private Date dataInsercao;
-    private Date dataExclusao;
+    private Date dataAlteracao;
 
-    public Cliente() {
-    }
-
-    public Cliente(Date dataExclusao) {
-        this.dataExclusao = dataExclusao;
-    }
-
-    public Cliente(Long id, String nome, String cpfcnpj, String dtNascimento, Date dataInsercao, Date dataExclusao) {
-        this.id = id;
-        this.nome = nome;
-        this.cpfcnpj = cpfcnpj;
-        this.dtNascimento = dtNascimento;
-        this.dataInsercao = dataInsercao;
-        this.dataExclusao = dataExclusao;
-    }
-
-
-    // getters e setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpfcnpj() {
-        return cpfcnpj;
-    }
-
-    public void setCpfcnpj(String cpfcnpj) {
-        this.cpfcnpj = cpfcnpj;
-    }
-
-    public String getDtNascimento() {
-        return dtNascimento;
-    }
-
-    public void setDtNascimento(String dtNascimento) {
-        this.dtNascimento = dtNascimento;
-    }
-
-    public Date getDataInsercao() {
-        return dataInsercao;
-    }
-
-    public void setDataInsercao(Date dataInsercao) {
-        this.dataInsercao = dataInsercao;
-    }
-
-    public Date getDataExclusao() {
-        return dataExclusao;
-    }
-
-    public void setDataAlteracao(Date dataExclusao) {
-        this.dataExclusao = dataExclusao;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
